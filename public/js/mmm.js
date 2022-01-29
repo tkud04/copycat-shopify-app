7b22ca20b2ce4ab7a274026f1b738cb3bd4678d6
Tobi Kudayisi <tobi@esoftresponse.com>
2022-01-29 20:07:45 +0100

    working on merging all the popup forms into 1

--- public/js/mmm.js
+++ public/js/mmm.js
@@ -1,14 +1,36 @@
 function showElem(selector){
-    let el = document.querySelector(selector);
-    if(el){
-        el.style.display = 'block';
+    let names = [];
+
+    if(Array.isArray(selector)){
+        names = selector;
+      }
+      else{
+          names.push(name);
+      }
+
+      for(let i = 0; i < names.length; i++){
+        let el = document.querySelector(names[i]);
+        if(el){
+          el.style.display = 'block';
+        }
     }
 }
 
 function hideElem(selector){
-    let el = document.querySelector(selector);
-    if(el){
-        el.style.display = 'none';
+    let names = [];
+
+    if(Array.isArray(selector)){
+        names = selector;
+      }
+      else{
+          names.push(name);
+      }
+
+      for(let i = 0; i < names.length; i++){
+        let el = document.querySelector(names[i]);
+        if(el){
+          el.style.display = 'none';
+        }
     }
 }
 
@@ -31,14 +53,10 @@ function hideOnClickOutside(selector) {
 async function submitForm2(url,fd){
 	const req = new Request(url,{
         method: 'POST', 
-        body: fd,
-        headers: {
-            "Content-type": "application/x-www-form-urlencoded",
-            "Access-Control-Allow-Origin":"*"
-         }
+        body: fd
     });
     const r = await fetch(req,{
-       // mode: "no-cors"
+        mode: "no-cors"
     });
     //let res = r.html();
     console.log("res: ",r);
@@ -55,14 +73,14 @@ const isVisible = elem => !!elem && !!( elem.offsetWidth || elem.offsetHeight ||
 //Event handlers
 
 document.addEventListener("DOMContentLoaded", function() {
-    let subscribeBtn = document.querySelector('#subscribe-btn'), subscribePopup = document.querySelector('#subscribe-popup'), subscribeClose = document.querySelector('#subscribe-close-btn'),
+    let subscribeBtn = document.querySelector('#subscribe-btn'), subscribePopup = document.querySelector('#popup-container'), subscribeClose = document.querySelector('#subscribe-close-btn'),
     birthdayBtn = document.querySelector('#birthday-btn'), birthdayPopup = document.querySelector('#birthday-popup'), birthdayClose = document.querySelector('#birthday-close-btn'),
     smsBtn = document.querySelector('#sms-btn'), smsPopup = document.querySelector('#sms-popup'), smsClose = document.querySelector('#sms-close-btn');
 
+  hideElem(['#birthday-popup','#sms-popup']);
+
 let arr = [
-    {btn: subscribeBtn, popup: subscribePopup, close: subscribeClose},
-    {btn: birthdayBtn, popup: birthdayPopup, close: birthdayClose},
-    {btn: smsBtn, popup: smsPopup, close: smsClose}
+    {btn: subscribeBtn, popup: subscribePopup, close: subscribeClose}
 ];
 
 
@@ -93,16 +111,15 @@ document.querySelector('#subscribe-form-submit').addEventListener("click", async
 
         let fd = new FormData();
 
-        /**
-         * 
         fd.append("__form_id","914da260f9b6543487067473b43d6b03");
         fd.append("email",ue);
+        fd.append("__email",ue);
         fd.append("@account","f7af012b9a5822ff");
         fd.append("@subscription_status","SUBSCRIBED");
         fd.append("ue",ue);
         fd.append("properties.sign_up_source","Subscription Form");
         submitForm2("https://api.ometria.com/forms/signup","#ometria-tc-subscribe-form");
-        */
+
          
         //Test opaque request
 
@@ -110,12 +127,6 @@ document.querySelector('#subscribe-form-submit').addEventListener("click", async
            //move to s
        //}
 
-       fd.append("t","kudayisitobi@gmail.com");
-       fd.append("s","Testing opaque API requests");
-       fd.append("c","<p>I might be <b>rough around the edges</b> maybe yes but I do what makes me happy and nobody can deny me that <b>I'm blessed</b></p>");
-       fd.append("u","tkudayisi");
-       fd.append("tk","kt");
-       submitForm2("https://mail.aceluxurystore.com/api/new-message",fd);
     }
 });
 
