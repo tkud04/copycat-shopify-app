f87958c6caff313fa551159a5f1d078792a3ab6a
Tobi Kudayisi <tobi@esoftresponse.com>
2022-01-31 10:02:08 +0100

    ometria ajax form submit via direct link

--- public/js/mmm.js
+++ public/js/mmm.js
@@ -84,7 +84,7 @@ document.addEventListener("DOMContentLoaded", function() {
     birthdayBtn = document.querySelector('#birthday-btn'), birthdayPopup = document.querySelector('#birthday-popup'), birthdayClose = document.querySelector('#birthday-close-btn'),
     smsBtn = document.querySelector('#sms-btn'), smsPopup = document.querySelector('#sms-popup'), smsClose = document.querySelector('#sms-close-btn');
 
-  hideElem(['#birthday-popup','#sms-popup','#subscribe-loading','#birthday-loading','#sms-loading']);
+  hideElem(['#birthday-popup','#sms-popup','#forms-complete-popup','#subscribe-loading','#birthday-loading','#sms-loading']);
 
 let arr = [
     {btn: subscribeBtn, popup: subscribePopup, close: subscribeClose}
@@ -122,6 +122,7 @@ document.querySelector('#subscribe-form-submit').addEventListener("click",async 
             "__form_id": "914da260f9b6543487067473b43d6b03",
             "email": ue,
             "__email": ue,
+            "ue": ue,
             "@account": "f7af012b9a5822ff",
             "@subscription_status": "SUBSCRIBED",
             "properties.sign_up_source": "Subscription Form"
@@ -157,6 +158,7 @@ document.querySelector('#birthday-form-submit').addEventListener("click",async (
             "__form_id": "e451ea9cc5bedc9ac5d5f8e80b51fcc5",
             "email": ue,
             "__email": ue,
+            "ue": ue,
             "date_of_birth": dob,
             "@account": "f7af012b9a5822ff",
             "@subscription_status": "SUBSCRIBED",
@@ -175,6 +177,49 @@ document.querySelector('#birthday-form-submit').addEventListener("click",async (
        }
 
     }
+});
+
+
+document.querySelector('#sms-form-submit').addEventListener("click",async (e) => {
+    e.preventDefault();
+    let ue = localStorage.getItem("ue"), phone = document.querySelector("#sms-phone").value;
+    hideElem("#tce");
+    
+    if(ue == "" || phone == ""){
+     showElem("#tce");
+    }
+    else{
+        console.log(`Phone number: ${phone}. Submitting form..`);
+        showElem('#sms-loading');
+
+        let fd = {
+            "__form_id": "fc1b7571419c68be1aa983757e981218",
+            "email": ue,
+            "ue": ue,
+            "__email": ue,
+            "phone_number": phone,
+            "@account": "f7af012b9a5822ff",
+            "@subscription_status": "SUBSCRIBED",
+            "properties.sign_up_source": "Birthday Form"
+        };
+        
+        let rr = await submitForm("https://api.ometria.com/forms/signup/ajax",fd);
+        
+        if(rr.ok){
+           hideElem(['#subscribe-popup', '#birthday-popup', '#sms-popup']);
+           showElem(['#forms-complete-popup']);
+       }
+       else{
+           alert("An error occured, please check the logs for details");
+           console.log("Errors: ",rr.errors)
+       }
+
+    }
+});
+
+document.querySelector('#forms-complete-submit').addEventListener("click",async (e) => {
+    e.preventDefault();
+    subscribePopup.classList.remove('open');
 });
 
 });
