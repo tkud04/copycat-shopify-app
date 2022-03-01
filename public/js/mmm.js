1214596a0a13c4698d46ccfba4e7ea691739a4db
Tobi Kudayisi <tobi@esoftresponse.com>
2022-03-01 21:50:51 +0100

    final fixes

--- public/js/mmm.js
+++ public/js/mmm.js
@@ -76,7 +76,7 @@ async function submitForm(url,fd){
 
 
 const isVisible = elem => !!elem && !!( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length ) // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js 
-let phoneValidation = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4,5})$/, emailValidation = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
+let phoneValidation = /^\(?[4]{0,2}?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4,5})$/, emailValidation = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
 
 
 //Event handlers
@@ -86,8 +86,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 let hasFilledForm = localStorage.getItem("copycat_hff");
 
-
-document.querySelector('#subscribe-btn').addEventListener("click", e => {
+let launchBtn = document.querySelector('#subscribe-btn');
+launchBtn?.addEventListener("click", e => {
     e.preventDefault();
     window.location = "#ometria-modal";
 });
@@ -119,19 +119,35 @@ document.querySelector('#date_of_birth').addEventListener("change", e => {
 //Form validation
 document.querySelector('#phone_number').addEventListener("change", e => {
     e.preventDefault();
-  let phone = document.querySelector("#phone_number").value, phoneValidationTest = phoneValidation.test(phone);
+  let phone = document.querySelector("#phone_number").value, phoneValidationTest = phoneValidation.test(phone),
+  bdayInput = document.querySelector("#bday-label"), emailInput = document.querySelector("#ue");
    hideElem(["#sfp"]);
    if(phone == "" || !phoneValidationTest){
      
     if(phone == ""){
         sfp.innerHTML = "Please fill in your phone number";
+        bdayInput.style = "margin-top: 10px";
         showElem("#sfp");
      } 
      if(!phoneValidationTest){
         sfp.innerHTML = "A valid phone number is required";
         showElem("#sfp");
-     } 
+     }
    }
+   else{
+    let fullCode = phone.substr(0,3), semiCode = phone.substr(0,2);
+    if(fullCode === "+44"){
+
+    }
+    else if(semiCode == "44"){
+        phone = "+" + phone.substr(0);
+    }
+    else{
+       phone = "+44" + phone.substr(1);
+    }
+    console.log(phone);
+    document.querySelector("#phone_number").value = phone;
+} 
 });
 
 document.querySelector('#ue').addEventListener("change", e => {
