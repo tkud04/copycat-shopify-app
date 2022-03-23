f7b330acbf54738459df6d4416e23ab960c69f64
Tobi Kudayisi <tobi@esoftresponse.com>
2022-03-23 15:12:24 +0100

    imporoved error handling

--- public/js/mmm2.js
+++ public/js/mmm2.js
@@ -63,143 +63,154 @@ function update(){
     $('#loading-span').html(x.email);
     showElem('#loading-2');
     //Update 
-    //Step 1: Fetch the provudct variant data from Shopify
+    if(x.pv.length > 5){
+        //Step 1: Fetch the provudct variant data from Shopify
     fetch(`/shopify-product-variants?pv=${x.pv}`)
-        .then(response => response.json())
-        .then(d => {
-            if(d.data){
-                let selected = d.data.find(i =>{ 
-                    let temp = x.pv.split("_");
-                    if(i.id == temp[1]) return i;
-                });
-    
-                if(selected){
-                    selected.email = x.email;
-                //console.log("selected: ",selected);
-                
-                //Step 2 - Fetch the customer custom fields from Ometria
-                fetch(`/ometria-get-customer?email=${encodeURIComponent(selected.email)}`)
-                .then(response => response.json())
-                .then(d => {
-                    if(d.data){
-                        console.log("[selected,d.data]: ",[selected,d.data]);
-                        let ometriaProperties = d.data.properties;
-                        let custom_fields = {}, toBeUpdated = false;
-                        
-                        switch(selected.title){
-                          case "5 ml":
-                          case "5ml":
-                              if(ometriaProperties.has_bought_5ml){
-                                custom_fields['has_bought_5ml'] = ometriaProperties['has_bought_5ml'] + 1;
-                              }
-                              else{
-                                custom_fields['has_bought_5ml'] = 1;
-                
-                              }
-                              toBeUpdated = true;
-                          break;
+    .then(response => response.json())
+    .then(d => {
+        if(d.data){
+            let selected = d.data.find(i =>{ 
+                let temp = x.pv.split("_");
+                if(i.id == temp[1]) return i;
+            });
 
-                          case "30 ml":
-                          case "30ml":
-                              if(ometriaProperties.has_bought_30ml){
-                                custom_fields['has_bought_30ml'] = ometriaProperties['has_bought_30ml'] + 1;
-                              }
-                              else{
-                                custom_fields['has_bought_30ml'] = 1;
-                                toBeUpdated = true;
-                              }
-                              toBeUpdated = true;
-                          break;
+            if(selected){
+                selected.email = x.email;
+            //console.log("selected: ",selected);
+            
+            //Step 2 - Fetch the customer custom fields from Ometria
+            fetch(`/ometria-get-customer?email=${encodeURIComponent(selected.email)}`)
+            .then(response => response.json())
+            .then(d => {
+                if(d.data){
+                    console.log("[selected,d.data]: ",[selected,d.data]);
+                    let ometriaProperties = d.data.properties;
+                    let custom_fields = {}, toBeUpdated = false;
+                    
+                    switch(selected.title){
+                      case "5 ml":
+                      case "5ml":
+                          if(ometriaProperties.has_bought_5ml){
+                            custom_fields['has_bought_5ml'] = ometriaProperties['has_bought_5ml'] + 1;
+                          }
+                          else{
+                            custom_fields['has_bought_5ml'] = 1;
+            
+                          }
+                          toBeUpdated = true;
+                      break;
 
-                          case "50 ml":
-                          case "50ml":
-                              if(ometriaProperties.has_bought_50ml){
-                                custom_fields['has_bought_50ml'] = ometriaProperties['has_bought_50ml'] + 1;
-                              }
-                              else{
-                                custom_fields['has_bought_50ml'] = 1;
-                                
-                              }
-                              toBeUpdated = true;
-                          break;
-
-                          case "100 ml":
-                          case "100ml":
-                              if(ometriaProperties.has_bought_100ml){
-                               custom_fields['has_bought_100ml'] = ometriaProperties['has_bought_100ml'] + 1;
-                            }
-                            else{
-                              custom_fields['has_bought_100ml'] = 1;
-                              
-                            }
+                      case "30 ml":
+                      case "30ml":
+                          if(ometriaProperties.has_bought_30ml){
+                            custom_fields['has_bought_30ml'] = ometriaProperties['has_bought_30ml'] + 1;
+                          }
+                          else{
+                            custom_fields['has_bought_30ml'] = 1;
                             toBeUpdated = true;
-                          break;
+                          }
+                          toBeUpdated = true;
+                      break;
+
+                      case "50 ml":
+                      case "50ml":
+                          if(ometriaProperties.has_bought_50ml){
+                            custom_fields['has_bought_50ml'] = ometriaProperties['has_bought_50ml'] + 1;
+                          }
+                          else{
+                            custom_fields['has_bought_50ml'] = 1;
+                            
+                          }
+                          toBeUpdated = true;
+                      break;
+
+                      case "100 ml":
+                      case "100ml":
+                          if(ometriaProperties.has_bought_100ml){
+                           custom_fields['has_bought_100ml'] = ometriaProperties['has_bought_100ml'] + 1;
                         }
-
-                         let payload = {
-                            customer_id: d.data.id,
-                            customer_email: selected.email,
-                            fields: custom_fields
-                          };
-
-                          let existingPayload = payloads.find(x => x.payload.customer_email === selected.email);
-                            console.log("[payload, existingPayload]: ",[payload,existingPayload]);
-
-                        if(existingPayload){
-                           if(payload.fields.has_bought_5ml){
-                               if(existingPayload.payload.fields.has_bought_5ml) ++existingPayload.payload.fields.has_bought_5ml;
-                               else existingPayload.payload.fields.has_bought_5ml = payload.fields.has_bought_5ml;
-                           }
-                           if(payload.fields.has_bought_30ml){
-                            if(existingPayload.payload.fields.has_bought_30ml) ++existingPayload.payload.fields.has_bought_30ml;
-                            else existingPayload.payload.fields.has_bought_30ml = payload.fields.has_bought_30ml;
-                           }
-                           if(payload.fields.has_bought_50ml){
-                            if(existingPayload.payload.fields.has_bought_50ml) ++existingPayload.payload.fields.has_bought_50ml;
-                            else existingPayload.payload.fields.has_bought_50ml = payload.fields.has_bought_50ml;
-                           }
-                           if(payload.fields.has_bought_100ml){
-                            if(existingPayload.payload.fields.has_bought_100ml) ++existingPayload.payload.fields.has_bought_100ml;
-                            else existingPayload.payload.fields.has_bought_100ml = payload.fields.has_bought_100ml;
-                        }
-                        } 
                         else{
-                            payloads.push({
-                                payload,
-                                toBeUpdated
-                            });  
+                          custom_fields['has_bought_100ml'] = 1;
+                          
                         }
-                        
-
-                        
-                        
-                        //Step 3 - Populate the appropriate custom field and update Ometria
-                       
-                        
-
-                            setTimeout(function(){
-                                //console.log("d.data: ", d.data);
-                                hideElem('#loading-2');
-                                ++updateCounter; 
-                                if(updateCounter < pv.length) update();
-                                else document.querySelector('#results-textarea').value = JSON.stringify(payloads);
-                                },1000); 
+                        toBeUpdated = true;
+                      break;
                     }
-                })
-                .catch(e => {
-                    console.log('error from ometria: ',e);
-                });
-                
-               }
 
-              
+                     let payload = {
+                        customer_id: d.data.id,
+                        customer_email: selected.email,
+                        fields: custom_fields
+                      };
+
+                      let existingPayload = payloads.find(x => x.payload.customer_email === selected.email);
+                        console.log("[payload, existingPayload]: ",[payload,existingPayload]);
+
+                    if(existingPayload){
+                       if(payload.fields.has_bought_5ml){
+                           if(existingPayload.payload.fields.has_bought_5ml) ++existingPayload.payload.fields.has_bought_5ml;
+                           else existingPayload.payload.fields.has_bought_5ml = payload.fields.has_bought_5ml;
+                       }
+                       if(payload.fields.has_bought_30ml){
+                        if(existingPayload.payload.fields.has_bought_30ml) ++existingPayload.payload.fields.has_bought_30ml;
+                        else existingPayload.payload.fields.has_bought_30ml = payload.fields.has_bought_30ml;
+                       }
+                       if(payload.fields.has_bought_50ml){
+                        if(existingPayload.payload.fields.has_bought_50ml) ++existingPayload.payload.fields.has_bought_50ml;
+                        else existingPayload.payload.fields.has_bought_50ml = payload.fields.has_bought_50ml;
+                       }
+                       if(payload.fields.has_bought_100ml){
+                        if(existingPayload.payload.fields.has_bought_100ml) ++existingPayload.payload.fields.has_bought_100ml;
+                        else existingPayload.payload.fields.has_bought_100ml = payload.fields.has_bought_100ml;
+                    }
+                    } 
+                    else{
+                        payloads.push({
+                            payload,
+                            toBeUpdated
+                        });  
+                    }
+                    
+
+                    
+                    
+                    //Step 3 - Populate the appropriate custom field and update Ometria
+                   
+                    
+
+                        setTimeout(function(){
+                            //console.log("d.data: ", d.data);
+                            hideElem('#loading-2');
+                            ++updateCounter; 
+                            if(updateCounter < pv.length) update();
+                            else document.querySelector('#results-textarea').value = JSON.stringify(payloads);
+                            },1000); 
+                }
+            })
+            .catch(e => {
+                console.log('error from ometria: ',e);
+            });
             
-            }
-            
-        })
-        .catch(e => {
-            console.log('error from shopify: ',e);
-        });;
+           }
+
+          
+        
+        }
+        
+    })
+    .catch(e => {
+        console.log('error from shopify: ',e);
+    });
+    }
+    else{
+        setTimeout(function(){
+            hideElem('#loading-2');
+            ++updateCounter; 
+            if(updateCounter < pv.length) update();
+            else document.querySelector('#results-textarea').value = JSON.stringify(payloads);
+            },1000); 
+    }
+    
 }
 
 function updateOmetria(){
